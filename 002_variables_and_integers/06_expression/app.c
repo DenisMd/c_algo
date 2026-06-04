35b324db22c7cc7e9bde744689c55d16c2f4f4fb
DenisMD <markovdenis2012@gmail.com>
2026-06-04 09:37:28 +0300

    002-expression example

--- 002_variables_and_integers/06_expression/app.c
+++ 002_variables_and_integers/06_expression/app.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 
+int super_func()
+{
+    return 5;
+}
+
+
 int main()
 {
     int a = 15;
     int b = 31;
 
-    int c = (a+b)*b - 5%a;
+    int c = (a+b)*b - 5%super_func();
     printf("%d %d %d\n", a, b, c);
 
     a = (b = c) + 1;
-    printf("%d %d %d\n", a, b, c);
+    printf("%d %d %d\n", a, b, super_func());
 
     return 0;
 }
