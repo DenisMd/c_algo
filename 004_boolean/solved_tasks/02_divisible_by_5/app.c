df5c2bc2610ff0296f04f0019bba445f697c964b
Dmarkov42 <markovdenis2012@gmail.com>
2026-06-27 15:10:20 +0300

    004 - fix

--- 004_boolean/solved_tasks/02_divisible_by_5/app.c
+++ 004_boolean/solved_tasks/02_divisible_by_5/app.c
@@ -5,7 +5,7 @@ int main()
     int n;
     scanf("%d", &n);
 
-    printf("%d\n", n % 5 != 0);
+    printf("%d\n", n % 5 == 0);
 
     return 0;
 }
--- 004_boolean/solved_tasks/08_projectile_range/app.c
+++ 004_boolean/solved_tasks/08_projectile_range/app.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <stdbool.h>
 
 int main()
 {
@@ -10,9 +11,9 @@ int main()
     assert(g > 0);
     assert(angle > 0 && angle < M_PI / 2);
 
-    int hits = v * v * sin(2 * angle) / g >= D;
+    bool is_hits = v * v * sin(2 * angle) / g >= D;
 
-    printf("%d\n", hits);
+    printf("%d\n", is_hits);
 
     return 0;
 }
